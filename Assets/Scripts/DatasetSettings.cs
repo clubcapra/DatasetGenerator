71e932a809693fd5df4e2fc7439dbb6ea44e1685
Marc-Olivier <islandx424@gmail.com>
2019-05-10 17:57:35 -0400

    Add basic generation with offser, rotation and distance

---
+++ Assets/Scripts/DatasetSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Settings", menuName = "Dataset", order = 1)]
+public class DatasetSettings : ScriptableObject
+{
+    public DatasetClass[] classes;
+}
+
+[System.Serializable]
+public class DatasetClass
+{
+    public Texture texture;
+    public string name;
+    public int objectClass;
+}
